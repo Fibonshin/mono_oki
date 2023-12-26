fba92c64ca1aa50d4d1e45c387f4a74f6faedf69
ふぃぼ <112852085+Fibonshin@users.noreply.github.com>
2023-12-26 20:52:01 +0900

    出力コードの最適化(もっと最適化できるけど)

--- bfgen.cpp
+++ bfgen.cpp
@@ -7,15 +7,19 @@ int main(){
     getline(cin,s);
     cout<<">>+++++[<+++++>-]<[<++++>-]";
     int cur=100;
+    int init=1;
     for(char c:s){
         int n=sqrt(abs(c-cur));
+        if(init)init=0;
+        else if(n)cout<<">";
         char a=c-cur>0?'+':'-';
         rep(i,n)cout<<'+';
-        cout<<"[<";
+        if(n)cout<<"[<";
         rep(i,n)cout<<a;
-        cout<<">-]<";
+        if(n)cout<<">-]<";
         rep(i,abs(cur-c)-n*n)cout<<a;
-        cout<<".>";
+        cout<<".";
         cur=c;
     }
+    cout<<endl;
 }
