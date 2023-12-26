10a8081ed784b42bb20eef6a3f34921308fd5285
ふぃぼ <112852085+Fibonshin@users.noreply.github.com>
2023-12-26 20:52:35 +0900

    バグがあったので修正

--- bfrun.cpp
+++ bfrun.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
@@ -7,40 +8,52 @@ int main(){
     cin>>s;
     vector<int> a(200000);
     int pos=100000;
+    int skip=0;
     stack<int> st;
     rep(i,s.size()){
+        sleep(0.01);
         switch (s.at(i))
         {
         case '<':
+            if(skip)break;
             pos--;
             break;
         case '>':
+            if(skip)break;
             pos++;
             break;
         case '+':
+            if(skip)break;
             a.at(pos)++;
             break;
         case '-':
+            if(skip)break;
             a.at(pos)--;
             break;
         case '[':
-            st.push(i);
+            if(!a.at(pos))skip++;
+            else st.push(i);
             break;
         case ']':
-            if(a.at(pos)){
-                i=st.top();
-            }else st.pop();
+            if(skip)skip--;
+            else{
+                i=st.top()-1;
+                st.pop();
+            }
             break;
         case ',':
+            if(skip)break;
             char c;
             cin>>c;
             a.at(pos)=c;
             break;
         case '.':
-            cout<<char(a.at(pos));
+            if(skip)break;
+            cout<<char(a.at(pos))<<flush;
             break;
         default:
             break;
         }
     }
+    cout<<endl;
 }
